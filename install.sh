8690b2324c074b57d60130c0c1d97f51b6575206
Brian Walborn <brian.walborn@underdogfantasy.com>
2025-05-07 14:02:24 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin main;
+
+function install() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "install.sh" \
+		-avh --no-perms . ~;
+
+	source ~/.zprofile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	install;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+
+	echo "";
+
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		install;
+	fi;
+fi;
+
+unset install;
